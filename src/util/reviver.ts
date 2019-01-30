a5439c1a0b8f747846b9cfb70d190e5367996c6c
Aubrey Collins <collinsa@allegheny.edu>
2019-01-30 11:43:09 -0500

    refactor dates parser

--- src/util/reviver.ts
+++ src/util/reviver.ts
@@ -1,8 +1,6 @@
 const JSONDatesReviver = (key: string, val: string) => {
-    const isDate = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(?:\:\d{2})*\.\d{2,}Z$/.test(val);
-    if (isDate) {
-        const dateObj = new Date(val);
-        return dateObj;
+    if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(?:\:\d{2})*\.\d{2,}Z$/.test(val)) {
+        return new Date(val);
     } else {
         return val;
     }
